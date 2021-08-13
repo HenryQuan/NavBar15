4a2283530a53fa3d82ae71f6f49cf1667119e18a
Yiheng <development.henryquan@gmail.com>
2021-08-13 11:35:52 +1000

    demo

---
+++ navbar15/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  navbar15
+//
+//  Created by Yiheng Quan on 13/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.barTintColor = .blue
+    }
+}
+
